2f850a7cd7323e5e352469cb2ba7400501579179
gongyexuan <gongyexuan@4399.com>
2024-11-06 17:29:43 +0800

    金山云SDK

--- XiaoJiuSDK.js
+++ XiaoJiuSDK.js
@@ -10,4 +10,10 @@
     script.id = 'infofe-kdocs-sdk'
     script.src = `//${scriptDomain}/xiao-jiu-sdk/infofe-kdocs-sdk.js?v=${new Date().getTime()}`
     document.body.appendChild(script)
+
+    const link = document.createElement('link')
+    link.rel = 'stylesheet'
+    link.id = 'infofe-kdocs-sdk-css'
+    link.href = `//${scriptDomain}/xiao-jiu-sdk/style.css?v=${new Date().getTime()}`
+    document.head.appendChild(link)
 })()
